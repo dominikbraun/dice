ca6b7e0a41da227e0fb426e53bc59c3511099738
Dominik Braun <Dominik.Braun@nbsp.de>
2019-11-26 16:31:48 +0100

    created Logger interface

---
+++ log/logger.go
@@ -0,0 +1,48 @@
+// Copyright 2019 The Dice Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package log
+
+import (
+	"github.com/sirupsen/logrus"
+	"io"
+)
+
+type Level uint32
+
+const (
+	DebugLevel Level = 0
+	InfoLevel  Level = 1
+	WarnLevel  Level = 2
+	ErrorLevel Level = 3
+)
+
+type Logger interface {
+	Debug(args ...interface{})
+	Debugf(format string, args ...interface{})
+	Info(args ...interface{})
+	Infof(format string, args ...interface{})
+	Warn(args ...interface{})
+	Warnf(format string, args ...interface{})
+	Error(args ...interface{})
+	Errorf(format string, args ...interface{})
+}
+
+func NewLogger(output io.Writer, level Level) Logger {
+	l := logrus.New()
+	l.SetOutput(output)
+	l.SetLevel(logrus.Level(level))
+
+	return l
+}
